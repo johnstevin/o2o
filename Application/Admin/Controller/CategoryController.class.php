39c602d8c6f6e368827d931a215ae936a0a41adb
zsh456 <1357442461@qq.com>
2015-05-21 18:12:06 +0800

    指定在view文件夹中显示分类列表，添加分类和编辑分类的模板文件

--- Application/Admin/Controller/CategoryController.class.php
+++ Application/Admin/Controller/CategoryController.class.php
@@ -4,17 +4,28 @@ namespace Admin\Controller;
 
 class CategoryController extends AdminController {
 
+    //分类管理模板
+    public function category(){
+
+    $this->display();
+    }
+    //分类列表
+    public function lis(){
+
+        $this->display();
+    }
+
     //分类添加
     public function add(){
 
-
+        $this->display();
 
     }
 
     //分类编辑
     public function edit(){
 
-
+        $this->display();
 
     }
 
