5f49cf7479ccd9f73d2d0d215749c279ddba7080
zsh456 <1357442461@qq.com>
2015-05-23 20:23:51 +0800

    commit

--- Application/Admin/Controller/CategoryController.class.php
+++ Application/Admin/Controller/CategoryController.class.php
@@ -1,23 +1,23 @@
 <?php
-
 namespace Admin\Controller;
 
 class CategoryController extends AdminController {
 
-    //分类管理模板
-    public function category(){
-
-    $this->display();
-    }
     //分类列表
-    public function lis(){
-
+    public function index(){
+    //查询出所有的分类
+        $tree=D("Category")->getTree(0,'id,sort,title,pid');
+//        $tree=M("Category")->select();
+//        $tree=D("Category")->info(0,'id,title,pid,sort,list_row,description,display,reply,check,extend,create_time,update_time,status,icon');
+        print_r($tree);
+        $this->assign('tree',$tree);
         $this->display();
     }
 
     //分类添加
     public function add(){
-
+//    $cateadd=M("Category")->create();
+//        var_dump($cateadd);
         $this->display();
 
     }
@@ -31,8 +31,6 @@ class CategoryController extends AdminController {
 
     //分类删除
     public function delete(){
-
-
 
     }
 
