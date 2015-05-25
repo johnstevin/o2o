203bb9d200c0ddc3b67f11b0d2c74c44b127a3c9
zqdn <342447229@qq.com>
2015-05-25 10:52:31 +0800

    规范了业务API

---
+++ Application/Apimember/Controller/ProductController.class.php
@@ -0,0 +1,71 @@
+<?php
+// +----------------------------------------------------------------------
+// | Created by stevin.
+// +----------------------------------------------------------------------
+// | Date: 2015-5-25
+// +----------------------------------------------------------------------
+namespace Api\Controller;
+
+/**
+ * 商品
+ * Class ProductController
+ * @package Api\Controller
+ */
+class ProductController extends ApiController {
+
+    /**
+     * 根据经纬度获取附近商家信息接口
+     * @param
+     * @author  stevin
+     */
+    public function getMerchantList(){
+
+    }
+
+    /**
+     * 根据获取的商家仓库的商品获取商品分类接口
+     * @param
+     * @author  stevin
+     */
+    public function getDepotCategory(){
+
+    }
+
+    /**
+     * 根据获取的商家仓库的商品获取商品品牌接口
+     * @param
+     * @author  stevin
+     */
+    public function getDepotBrand(){
+
+    }
+
+    /**
+     * 根据获取的商家仓库的商品获取商品规格接口
+     * @param
+     * @author  stevin
+     */
+    public function getDepotNorms(){
+
+    }
+
+    /**
+     * 根据分类（品牌、规格）获取商品信息接口
+     * @param
+     * @author  stevin
+     */
+    public function getProductList(){
+
+    }
+
+    /**
+     * 商品详细
+     * @param
+     * @author  stevin
+     */
+    public function getProductDetail(){
+
+    }
+
+
+}
---
+++ Application/Apimerchant/Controller/EmptyController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Api\Controller;
+
+/**
+ * 空模块，主要用于显示404页面，请不要删除
+ */
+class EmptyController extends ApiController{
+	//没有任何方法，直接执行HomeController的_empty方法
+	//请不要删除该控制器
+}
