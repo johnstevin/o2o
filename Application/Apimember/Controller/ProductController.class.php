efa455af00fdc3f5c188f841b54af39a972e60ba
WJ <sillyemperor@github.com>
2015-05-25 11:58:22 +0800

    准备合并

--- Application/Apimember/Controller/ProductController.class.php
+++ Application/Apimember/Controller/ProductController.class.php
@@ -5,6 +5,8 @@
 // | Date: 2015-5-25
 // +----------------------------------------------------------------------
 namespace Api\Controller;
+use Think\Exception;
+use Common\Model\MerchantShop;
 
 /**
  * 商品
@@ -15,11 +17,21 @@ class ProductController extends ApiController {
 
     /**
      * 根据经纬度获取附近商家信息接口
-     * @param
-     * @author  stevin
+     * @param double $lat 查询中心维度，必须是百度坐标
+     * @param double $lng 查询中心经度，必须是百度坐标
+     * @param double $range 查询半径，单位米，缺省100米
+     * @param null|string|array words 关键字，w1,w2... 在title以及description字段中查找
+     * @param string words_op  or|and，关键字组合方式
+     * @param int $type 商家门店类型，可选0-所有类型，1-超市，2-生鲜，3-洗车，4-送水，缺省0
+     * @author  stevin WangJiang
      */
-    public function getMerchantList(){
-
+    public function getMerchantList($lat, $lng, $range = 100,$words=null,$words_op='or',$type='0'){
+        try{
+            $m=new MerchantShop();
+            $this->apiSuccess(array('data'=>$m->getList($lat, $lng, $range,$words,$words_op,$type)));
+        }catch (Exception $ex){
+            $this->apiError(50002,$ex->getMessage());
+        }
     }
 
     /**
--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,6 +1,4 @@
 <?php
-<<<<<<< HEAD
-
 /**
  * 检查IP是否合法
  * @param string $ip 要检查的IP地址
@@ -10,5 +8,4 @@ function checkIpFormat($ip)
 {
     return filter_var($ip, FILTER_VALIDATE_IP) ? true : false;
 }
-=======
->>>>>>> origin/feature/stevin
+
