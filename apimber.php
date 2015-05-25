b1b200ccf1a7a0592e47990deb98477835cbc388
zqdn <342447229@qq.com>
2015-05-25 11:53:34 +0800

    .

---
+++ apimber.php
@@ -0,0 +1,14 @@
+<?php
+
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+/**
+ * 系统调试设置
+ * 项目正式部署后请设置为false
+ */
+define('APP_DEBUG', true );
+define('BIND_MODULE','Apimember');
+define('APP_PATH', './Application/');
+define('RUNTIME_PATH', './Runtime/');
+
+require './ThinkPHP/ThinkPHP.php';
