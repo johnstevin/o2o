dbdfac361a38ca13fadf52d567f696c8b04ea860
zqdn <342447229@qq.com>
2015-05-26 16:27:31 +0800

    合并王江代码提交

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -9,3 +9,31 @@ function checkIpFormat($ip)
     return filter_var($ip, FILTER_VALIDATE_IP) ? true : false;
 }
 
+/**
+ * @param $words
+ * @param $words_op
+ * @param $flds
+ * @param $map
+ * @return mixed
+ */
+function build_words_query($words, $words_op, $flds, &$map)
+{
+    //TODO:奇葩问题，传入的参数是'or'时，TP会转换成'or '
+    $words_op=trim($words_op);
+
+    $nw = count($words);
+    $nf = count($flds);
+    $where_kws = null;
+    for ($i = 0; $i < $nf; $i++) {
+        $val = array();
+        for ($j = 0; $j < $nw; $j++) {
+            $val[] = array('like', '%' . $words[$j] . '%');
+        }
+        $val[] = $words_op;
+        //$val['_logic']='or';
+        $where_kws[$flds[$i]] = $val;
+    }
+    $where_kws['_logic'] = 'or';
+    $map['_complex'] = $where_kws;
+    return $map;
+}
