237e79989476510196f262a040866ecad1c2d654
zsh456 <1357442461@qq.com>
2015-05-27 10:07:04 +0800

    封装了设置一条或者多条数据的状态的函数：setStatus

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Admin\Controller;
 use Think\Controller;
 
@@ -11,6 +10,116 @@ class AdminController extends Controller {
 
     protected function _initialize(){
 
+    }
+
+    /**
+     * 对数据表中的单行或多行记录执行修改 GET参数id为数字或逗号分隔的数字
+     *
+     * @param string $model 模型名称,供M函数使用的参数
+     * @param array $data 修改的数据
+     * @param array $where 查询时的where()方法的参数
+     * @param array $msg 执行正确和错误的消息 array('success'=>'','error'=>'', 'url'=>'','ajax'=>false)
+     *                     url为跳转页面,ajax是否ajax方式(数字则为倒数计时秒数)
+     *
+     * @author 朱亚杰  <zhuyajie@topthink.net>
+     */
+    final protected function editRow($model, $data, $where, $msg)
+    {
+        $id = array_unique((array)I('id', 0));
+        $id = is_array($id) ? implode(',', $id) : $id;
+        //如存在id字段，则加入该条件
+        $fields = M($model)->getDbFields();
+        if (in_array('id', $fields) && !empty($id)) {
+            $where = array_merge(array('id' => array('in', $id)), (array)$where);
+        }
+        $msg = array_merge(array('success' => '操作成功！', 'error' => '操作失败！', 'url' => '', 'ajax' => IS_AJAX), (array)$msg);
+        if (M($model)->where($where)->save($data) !== false) {
+            $this->success($msg['success'], $msg['url'], $msg['ajax']);
+        } else {
+            $this->error($msg['error'], $msg['url'], $msg['ajax']);
+        }
+    }
+    /**
+     * 禁用条目
+     * @param string $model 模型名称,供D函数使用的参数
+     * @param array $where 查询时的 where()方法的参数
+     * @param array $msg 执行正确和错误的消息,可以设置四个元素 array('success'=>'','error'=>'', 'url'=>'','ajax'=>false)
+     *                     url为跳转页面,ajax是否ajax方式(数字则为倒数计时秒数)
+     *
+     * @author 朱亚杰  <zhuyajie@topthink.net>
+     */
+    protected function forbid($model, $where = array(), $msg = array('success' => '状态禁用成功！', 'error' => '状态禁用失败！'))
+    {
+        $data = array('status' => 0);
+        $this->editRow($model, $data, $where, $msg);
+    }
+    /**
+     * 恢复条目
+     * @param string $model 模型名称,供D函数使用的参数
+     * @param array $where 查询时的where()方法的参数
+     * @param array $msg 执行正确和错误的消息 array('success'=>'','error'=>'', 'url'=>'','ajax'=>false)
+     *                     url为跳转页面,ajax是否ajax方式(数字则为倒数计时秒数)
+     *
+     * @author 朱亚杰  <zhuyajie@topthink.net>
+     */
+    protected function resume($model, $where = array(), $msg = array('success' => '状态恢复成功！', 'error' => '状态恢复失败！'))
+    {
+        $data = array('status' => 1);
+        $this->editRow($model, $data, $where, $msg);
+    }
+    /**
+     * 还原条目
+     * @param string $model 模型名称,供D函数使用的参数
+     * @param array $where 查询时的where()方法的参数
+     * @param array $msg 执行正确和错误的消息 array('success'=>'','error'=>'', 'url'=>'','ajax'=>false)
+     *                     url为跳转页面,ajax是否ajax方式(数字则为倒数计时秒数)
+     * @author huajie  <banhuajie@163.com>
+     */
+    protected function restore($model, $where = array(), $msg = array('success' => '状态还原成功！', 'error' => '状态还原失败！'))
+    {
+        $data = array('status' => 1);
+        $where = array_merge(array('status' => -1), $where);
+        $this->editRow($model, $data, $where, $msg);
+    }
+    /**
+     * 条目假删除
+     * @param string $model 模型名称,供D函数使用的参数
+     * @param array $where 查询时的where()方法的参数
+     * @param array $msg 执行正确和错误的消息 array('success'=>'','error'=>'', 'url'=>'','ajax'=>false)
+     *                     url为跳转页面,ajax是否ajax方式(数字则为倒数计时秒数)
+     *
+     * @author 朱亚杰  <zhuyajie@topthink.net>
+     */
+    protected function delete($model, $where = array(), $msg = array('success' => '删除成功！', 'error' => '删除失败！'))
+    {
+        $data['status'] = -1;
+        $this->editRow($model, $data, $where, $msg);
+    }
+    /**
+     * 设置一条或者多条数据的状态
+     */
+    public function setStatus($Model = CONTROLLER_NAME)
+    {
+        $ids = I('request.ids');
+        $status = I('request.status');
+        if (empty($ids)) {
+            $this->error('请选择要操作的数据');
+        }
+        $map['id'] = array('in', $ids);
+        switch ($status) {
+            case -1 :
+                $this->delete($Model, $map, array('success' => '删除成功', 'error' => '删除失败'));
+                break;
+            case 0  :
+                $this->forbid($Model, $map, array('success' => '禁用成功', 'error' => '禁用失败'));
+                break;
+            case 1  :
+                $this->resume($Model, $map, array('success' => '启用成功', 'error' => '启用失败'));
+                break;
+            default :
+                $this->error('参数错误');
+                break;
+        }
     }
 
 }
