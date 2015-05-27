6b7b03f4fa67352c51f71fe3dc6139812ddced59
zsh456 <1357442461@qq.com>
2015-05-27 10:04:49 +0800

    添加了数据库相关设置

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -29,4 +29,13 @@ return array(
     'TMPL_ACTION_SUCCESS'   =>  MODULE_PATH.'View/Public/success.html', // 默认成功跳转对应的模板文件
     'TMPL_EXCEPTION_FILE'   =>  MODULE_PATH.'View/Public/exception.html',// 异常页面的模板文件
 
+    /* 数据库设置 */
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '192.168.31.139', // 服务器地址
+    'DB_NAME'               =>  'o2o',          // 数据库名
+    'DB_USER'               =>  'o2odb',      // 用户名
+    'DB_PWD'                =>  '123456',          // 密码
+    'DB_PREFIX'             =>  'sq_',    // 数据库表前缀
+
+
 );
