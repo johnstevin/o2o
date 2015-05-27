c61e196a991bfef98cd3a48caf66d1ec76126df3
zsh456 <1357442461@qq.com>
2015-05-27 10:14:01 +0800

    封装了获取分类详细信息：info、获取当前分类信息：getTree以及更新分类：update 的函数

--- Application/Admin/Model/CategoryModel.class.php
+++ Application/Admin/Model/CategoryModel.class.php
@@ -16,31 +16,52 @@ class CategoryModel extends Model{
         return $this->field($field)->where($map)->find();
     }
 
-    /*获取分类树，指定分类则返回指定分类及其子分类，不指定则返回所有分类树
-    @parm $id     分类ID
-    @parm fields  查询字段
-    @parm array   分类树*/
-
-    public function getTree($id=0,$field=true){
-    //获取当前分类信息
-    if($id){
-        $info=$this->info($id);
-        $id=$info['id'];
-    }
-        //获取所有分类
-        $map=array();
-        $list=$this->field($field)->where($map)->order('sort')->select();
-        $list=list_to_tree($list,$pk='id',$pid='pid',$root=$id);
-
-        //获取返回数据
-        if(isset($info)){//指定分类则返回当前分类及其子分类
-            $info['_']=$list;
-        }else{//否则则返回所有分类
-            $info=$list;
+    /**
+     * 获取分类树，指定分类则返回指定分类极其子分类，不指定则返回所有分类树
+     * @param  integer $id    分类ID
+     * @param  boolean $field 查询字段
+     * @return array          分类树
+     * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+     */
+    public function getTree($id = 0, $field = true){
+        /* 获取当前分类信息 */
+        if($id){
+            $info = $this->info($id);
+            $id   = $info['id'];
         }
+
+        /* 获取所有分类 */
+        $list = $this->field($field)->order('sort')->select();
+        $list = list_to_tree($list, $pk = 'id', $pid = 'pid', $child = 'child', $root = $id);
+
+        /* 获取返回数据 */
+        if(isset($info)){ //指定分类则返回当前分类极其子分类
+            $info['_'] = $list;
+        } else { //否则返回所有分类
+            $info = $list;
+        }
+
         return $info;
     }
 
+    /**
+     * 更新分类信息
+     * @return boolean 更新状态
+     * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+     */
+    public function update(){
+        $data = $this->create();
+        if(!$data){ //数据对象创建错误
+            return false;
+        }
 
+        /* 添加或更新数据 */
+        if(empty($data['id'])){
+            $res = $this->add();
+        }else{
+            $res = $this->save();
+        }
 
+        return $res;
+    }
 }
