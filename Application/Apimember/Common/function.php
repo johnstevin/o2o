8599a5a9ec0700cc54b36290576f884a52884bf8
WJ <sillyemperor@github.com>
2015-05-28 11:33:24 +0800

    增加注释

--- Application/Apimember/Common/function.php
+++ Application/Apimember/Common/function.php
@@ -2,16 +2,19 @@
 
 
 /**
- * @param $shopIds
- * @return array
+ * 通过数值构建SQL参数绑定
+ * @author WangJiang
+ * @param array $list 条件数组
+ * @param string $prefix 参数名称前缀
+ * @return array $bindNames 参数名称, $bindValues参数绑定用于bind调用
  */
-function build_sql_bind($shopIds)
+function build_sql_bind($list,$prefix='bindName')
 {
     $bindNames = [];
     $bindValues = [];
 
-    foreach ($shopIds as $i => $id) {
-        $name = ':id' . $i;
+    foreach ($list as $i => $id) {
+        $name = ':'.$prefix . $i;
         $bindNames[] = $name;
         $bindValues[$name] = $id;
     }
