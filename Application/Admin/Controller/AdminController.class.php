a25ec9b97439d1e5858474d2c4925136a5bd3f1c
johnstevin <abangsir@sina.com>
2015-05-29 21:56:35 +0800

    .

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -37,8 +37,66 @@ class AdminController extends Controller{
             }
         }
 
-        
+        // 检测系统权限
+        if(!IS_ROOT){
+            $access =   $this->accessControl();
+            if ( false === $access ) {
+                $this->error('403:禁止访问');
+            }elseif(null === $access ){
+                //检测访问权限
+                $rule  = strtolower(MODULE_NAME.'/'.CONTROLLER_NAME.'/'.ACTION_NAME);
+                if ( !$this->checkRule($rule,array('in','1,2')) ){
+                    $this->error('未授权访问!');
+                }else{
+                    // 检测分类及内容有关的各项动态权限
+                    $dynamic    =   $this->checkDynamic();
+                    if( false === $dynamic ){
+                        $this->error('未授权访问!');
+                    }
+                }
+            }
+        }
 
+
+    }
+
+    /**
+     * action访问控制,在 **登陆成功** 后执行的第一项权限检测任务
+     *
+     * @return boolean|null  返回值必须使用 `===` 进行判断
+     *
+     *   返回 **false**, 不允许任何人访问(超管除外)
+     *   返回 **true**, 允许任何管理员访问,无需执行节点权限检测
+     *   返回 **null**, 需要继续执行节点权限检测决定是否允许访问
+     */
+    final protected function accessControl(){
+        $allow = C('ALLOW_VISIT');
+        $deny  = C('DENY_VISIT');
+        $check = strtolower(CONTROLLER_NAME.'/'.ACTION_NAME);
+        if ( !empty($deny)  && in_array_case($check,$deny) ) {
+            return false;//非超管禁止访问deny中的方法
+        }
+        if ( !empty($allow) && in_array_case($check,$allow) ) {
+            return true;
+        }
+        return null;//需要检测节点权限
+    }
+
+    /**
+     * 权限检测
+     * @param string  $rule    检测的规则
+     * @param string  $mode    check模式
+     * @return boolean
+     */
+    final protected function checkRule($rule, $type=AuthRuleModel::RULE_URL, $mode='url'){
+        static $Auth    =   null;
+        if (!$Auth) {
+            $Auth       =   new \Think\Auth();
+        }
+        if(!$Auth->check($rule,UID,$type,$mode)){
+            return false;
+        }
+        return true;
     }
 
     /**
