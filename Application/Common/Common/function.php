5fecbdf1c402872d62244b231051178b8e4e82a8
WJ <sillyemperor@github.com>
2015-06-01 13:46:16 +0800

    修改移动build_sql_bind函数

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -333,3 +333,22 @@ function int_to_string(&$data,$map=array('status'=>array(1=>'正常',-1=>'删除
     }
     return $data;
 }
+
+/**
+ * 通过数值构建SQL参数绑定
+ * @author WangJiang
+ * @param array $list 条件数组
+ * @param array $bindNames
+ * @param array $bindValues
+ * @param string $prefix 参数名称前缀
+ * @return array $bindNames 参数名称, $bindValues参数绑定用于bind调用
+ */
+function build_sql_bind($list,$bindValues=[],$prefix='bindName')
+{
+    foreach ($list as $i => $id) {
+        $name = ":$prefix" . $i;
+        $bindNames[] = $name;
+        $bindValues[$name] = $id;
+    }
+    return array($bindNames, $bindValues);
+}
