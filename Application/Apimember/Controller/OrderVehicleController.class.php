797da805f5da5c3026e8ddb5366d76a95163cc45
wj <346294202@qq.com>
2015-06-19 18:12:10 +0800

    订单初步

---
+++ Application/Apimember/Controller/OrderVehicleController.class.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuhui
+ * Date: 6/19/15
+ * Time: 2:20 PM
+ */
+
+namespace Apimember\Controller;
+
+
+use Common\Model\OrderVehicleModel;
+
+class OrderVehicleController extends ApiController
+{
+    /**
+     * 获得用户订单
+     * @author WangJiang
+     * @param null $status
+     * @param null $orderCode
+     * @param int $page
+     * @param int $pageSize
+     * @return json
+     * ``` json
+     * {
+     *      "success":<>,
+     *      "error_code":<>,
+     *      "message":<>,
+     *      "data":[
+     *          {
+     *              {
+     *                  "lnglat":[经度,纬度],
+     *                  "id": "<订单ID>",
+     *                  "order_code": "<订单编号>",
+     *                  "user_id": "<用户ID>",
+     *                  "shop_id": "<商铺ID>",
+     *                  "status": "<状态，0-未分配，1-已分配，2-已接单，3-处理中，4-处理完，5-订单结束，6订单取消>",
+     *                  "worker_id": "<洗车工ID，为空表示还没有分配>",
+     *                  "address": "<车辆地址>",
+     *                  "car_number": "<车牌号>",
+     *                  "price": "<洗车价格>",
+     *                  "user_picture_ids": "<用户上传的图片，多个用[,]隔开>",
+     *                  "worder_picture_ids": "<洗车工上传的图片，多个用[,]隔开>",
+     *                  "add_time": "<新增时间>",
+     *                  "update_time": "<修改时间>"
+     *              },...
+     *          }
+     *      ]
+     * }
+     * ```
+     * <pre>调用样例 GET apimber.php?s=OrderVehicle/getList&accesstoken=104</pre>
+     * ``` json
+     *{
+     *
+     *      "success": true,
+     *      "error_code": 0,
+     *      "message": "",
+     *      "data":[
+     *          {
+     *              "lnglat":
+     *              [
+     *                  120,
+     *                  30
+     *              ],
+     *              "id": "20806",
+     *              "order_code": "1000",
+     *              "user_id": "104",
+     *              "shop_id": "0",
+     *              "status": "5",
+     *              "worker_id": "10168",
+     *              "address": "",
+     *              "car_number": "A12345",
+     *              "price": "15.00",
+     *              "user_picture_ids": "",
+     *              "worder_picture_ids": "",
+     *              "add_time": "1388505600",
+     *              "update_time": "1388505600"
+     *
+     *          },
+     *          {
+     *          "lnglat":
+     *          [
+     *              120,
+     *              30
+     *          ],
+     *          "id": "20811",
+     *          "order_code": "1000",
+     *          "user_id": "104",
+     *          "shop_id": "0",
+     *          "status": "0",
+     *          "worker_id": "10146",
+     *          "address": "",
+     *          "car_number": "A12345",
+     *          "price": "15.00",
+     *          "user_picture_ids": "",
+     *          "worder_picture_ids": "",
+     *          "add_time": "1388592000",
+     *          "update_time": "1388592000"
+     *          }
+     *      ]
+     * }
+     * ```
+     */
+    public function getList($status = null, $orderCode = null, $page = 1, $pageSize = 10)
+    {
+        try {
+            $pageSize > 50 and $pageSize = 50;
+            $page--;
+            $page *= $pageSize;
+            $uid = $this->getUserId();
+
+            $m = D('OrderVehicle');
+            $data = $m
+                ->field(['st_astext(lnglat) as lnglat',
+                    'id',
+                    'order_code',
+                    'user_id',
+                    'shop_id',
+                    'status',
+                    'worker_id',
+                    'address',
+                    'car_number',
+                    'price',
+                    'user_picture_ids',
+                    'worder_picture_ids',
+                    'add_time',
+                    'update_time',
+                ])
+                ->where(['user_id' => $uid])->limit($page, $pageSize)->select();
+
+            //print_r($m->getLastSql());die;
+
+            $this->apiSuccess(['data' => $data], '');
+        } catch (\Exception $ex) {
+            $this->apiError(51002, $ex->getMessage());
+        }
+    }
+
+    /**
+     * 创建订单，POST参数
+     * <pre>
+     * 参数：
+     * worker_id 洗车工ID
+     * address 车辆地址，必须
+     * lnglat  车辆位置，格式为'lng lat'，必须
+     * car_number 车牌号，必须
+     * user_picture_ids  用户照片ID，用','隔开，必须
+     * </pre>
+     * @author WangJiang
+     * @return json
+     */
+    public function create(){
+        /*
+         * 洗车下单流程：
+         *  if 设置worker_id then 将status设置为已分配，插入记录，向worker_id推送消息
+         *  else 很据lnglat计算周围空闲worker_id
+         *    if 找到了 then 选择第一个，将status设置为已分配，插入记录，向worker_id推送消息
+         *    else 将status设置为未分配，插入记录
+         */
+        try {
+            if (IS_POST) {
+                $model = new OrderVehicleModel();
+                if (!($data=$model->create()))
+                    E('参数传递失败');
+                $data['user_id']=$this->getUserId();
+                if(!array_key_exists('worker_id',$data) or empty($data['worker_id'])){
+                    $wid=$model->find_avalable_worker($data);
+                    if(is_null($wid))
+                        $data['status']=OrderVehicleModel::STATUS_NO_WORKER;
+                    else{
+                        $data['status']=OrderVehicleModel::STATUS_HAS_WORKER;
+                        $data['worker_id']=$wid;
+                    }
+                }else
+                    $data['status']=OrderVehicleModel::STATUS_HAS_WORKER;
+                //判断是否推送消息
+                $sendMsg=$data['status']==OrderVehicleModel::STATUS_HAS_WORKER;
+
+                $model->data($data);
+                $this->apiSuccess(['id' => intval($model->insert(function() use ($sendMsg,$wid){
+                    if($sendMsg){
+                        //TODO 实现消息推送$wid
+                    }
+                }))],'');
+            } else
+                E('非法调用');
+        } catch (\Exception $ex) {
+            $this->apiError(51021, $ex->getMessage());
+        }
+    }
+
+    /**
+     * 修改订单，POST参数
+     * @author WangJiang
+     * @return json
+     */
+    public function update(){
+
+    }
+}
