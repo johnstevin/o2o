e9164237decbb838d42e4d8f69762bcc47a8e0e0
nugh <1276163661@qq.com>
2015-06-24 14:04:25 +0800

    修改产品唯一

--- Application/Admin/Model/ProductModel.class.php
+++ Application/Admin/Model/ProductModel.class.php
@@ -22,6 +22,8 @@ class ProductModel extends Model
      * @var array
      */
     protected $_validata = array(
+
+        array('number', 'unique', '该商品的已经存在', self::MUST_VALIDATE, 'regex', self::MODEL_INSERT),
         array('title', 'require', '必须设置商品标题', self::MUST_VALIDATE, 'regex', self::MODEL_INSERT),
         array('price', 'currency', '商品价格必须是货币形式', self::MUST_VALIDATE, 'regex', self::MODEL_INSERT),
     );
