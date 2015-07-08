94c6c2a1b8510adb8dfbabeb630d21e3dd755596
NFF <niefufeng@gmail.com>
2015-07-08 11:26:46 +0800

    update

--- Application/Apimerchant/Controller/OrderController.class.php
+++ Application/Apimerchant/Controller/OrderController.class.php
@@ -6,6 +6,7 @@
 // +----------------------------------------------------------------------
 namespace Apimerchant\Controller;
 
+use Common\Model\OrderModel;
 /**
  * 商家订单管理
  * Class OrderController
@@ -38,6 +39,39 @@ class OrderController extends ApiController {
      */
     public function getOrderDo(){
 
+    }
+
+    /**
+     * ## 完成订单
+     * @author Fufeng Nie <niefufeng@gmail.com>
+     * @param int $id 订单ID
+     */
+    public function completeOrder($id)
+    {
+        $this->apiSuccess(['data' => OrderModel::getInstance()->CompleteOrder($id, $this->getUserId())]);
+    }
+
+    /**
+     * ## 删除订单
+     * @author Fufeng Nie <niefufeng@gmail.com>
+     *
+     * @param int $id 订单ID
+     */
+    public function deleteOrder($id)
+    {
+        $this->apiSuccess(['data' => OrderModel::getInstance()->DeleteOrder($id, $this->getUserId())]);
+    }
+
+    /**
+     * ## 商家确认订单
+     * @author Fufeng Nie <niefufeng@gmail.com>
+     *
+     * @param int $id 订单ID，必传参数
+     * @param bool $confirm 是否确认订单，可选参数，默认为确认
+     */
+    public function merchantConfirmOrder($id, $confirm = true)
+    {
+        $this->apiSuccess(['data' => OrderModel::getInstance()->MerchantConfirmOrder($id, $confirm, $this->getUserId())]);
     }
 
 }
